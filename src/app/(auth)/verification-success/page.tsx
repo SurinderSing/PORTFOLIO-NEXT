a1cea63d9bbf4960721aaf0a54d77117d3ed5647
Surinder Singh <ssurindersingh100@gmail.com>
2025-02-27 16:50:03 +0530

    feat: Add verification success page for email verification flow

---
+++ src/app/(auth)/verification-success/page.tsx
@@ -0,0 +1,9 @@
+export default function VerificationSuccess() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-lg text-green-300">
+        Your email has been verified! 😉
+      </h1>
+    </div>
+  );
+}
