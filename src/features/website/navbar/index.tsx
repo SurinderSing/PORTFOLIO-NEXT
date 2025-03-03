3f0ebf159a2c34546d3e7b48a2a0df7ddcf6f457
Surinder Singh <ssurindersingh100@gmail.com>
2025-03-03 23:30:53 +0530

    style: Adjust Navbar padding and gap for improved spacing

--- src/features/website/navbar/index.tsx
+++ src/features/website/navbar/index.tsx
@@ -5,7 +5,7 @@ import useData from './useData';
 const Navbar: React.FC = () => {
   const [menuItems] = useData();
   return (
-    <div className="flex gap-10 items-center justify-center max-w-[503px] justify-self-end py-3 px-5 rounded-2xl shadow-custom-1">
+    <div className="flex gap-8 items-center justify-center max-w-[503px] justify-self-end py-3 px-6 rounded-2xl shadow-custom-1">
       {menuItems.map((menuItem, index: number) => (
         <MenuBtn key={menuItem.id + '-' + index} menuItem={menuItem} />
       ))}
