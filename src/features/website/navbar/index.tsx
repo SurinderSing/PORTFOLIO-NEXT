fbbb629bb9709b5eb9aa09e527c30c125e3ecc17
Surinder Singh <ssurindersingh100@gmail.com>
2025-03-04 10:59:30 +0530

    style: Add background color to Navbar
    - Applied tertiary background color to Navbar
    - Removed redundant justify-center class

--- src/features/website/navbar/index.tsx
+++ src/features/website/navbar/index.tsx
@@ -5,7 +5,7 @@ import useData from './useData';
 const Navbar: React.FC = () => {
   const [menuItems] = useData();
   return (
-    <div className="flex gap-8 items-center justify-center max-w-[503px] justify-self-end py-2 px-6 rounded-2xl shadow-custom-1">
+    <div className="flex gap-8 items-center justify-center justify-self-end max-w-[503px] py-2 px-6 rounded-2xl bg-tertiary shadow-custom-1">
       {menuItems.map((menuItem, index: number) => (
         <MenuBtn key={menuItem.id + '-' + index} menuItem={menuItem} />
       ))}
