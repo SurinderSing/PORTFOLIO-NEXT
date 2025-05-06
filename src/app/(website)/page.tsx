dac07b16c4e738a845b142f90039ceb58468a998
Surinder Singh <ssurindersingh100@gmail.com>
2025-05-06 23:26:43 +0530

    Enhances homepage layout and resume button
    Improves homepage by structuring content into sections, adding
    a new title and placeholder for future content. Updates the
    resume download button to include an icon and improve
    styling for better user experience.

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -4,15 +4,25 @@ export default function Home() {
   return (
     <main className="w-full">
       <PageProvider title="ABOUT ME">
-        <p className="para-2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi non
-          ea vitae a. Ullam reiciendis, voluptatibus amet aliquam voluptatem a
-          quos nihil provident porro similique architecto libero dolorum non
-          soluta. Accusamus magnam ipsa aliquid laboriosam ad culpa reiciendis,
-          magni, aspernatur odit eaque, veniam cupiditate nam reprehenderit
-          eveniet dolorem modi nisi tenetur necessitatibus quia vitae veritatis.
-          Eaque nemo veniam eveniet rem! Facere quos molestiae, neque aliquam
-        </p>
+        {/* About Summary */}
+        <div className="mb-3">
+          <p className="para-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+            non ea vitae a. Ullam reiciendis, voluptatibus amet aliquam
+            voluptatem a quos nihil provident porro similique architecto libero
+            dolorum non soluta. Accusamus magnam ipsa aliquid laboriosam ad
+            culpa reiciendis, magni, aspernatur odit eaque, veniam cupiditate
+            nam reprehenderit eveniet dolorem modi nisi tenetur necessitatibus
+            quia vitae veritatis. Eaque nemo veniam eveniet rem! Facere quos
+            molestiae, neque aliquam
+          </p>
+        </div>
+
+        {/* What I do! */}
+        <div>
+          <h2 className="mb-1.5">What I do!</h2>
+          <div></div>
+        </div>
       </PageProvider>
     </main>
   );
--- src/components/website/profile-side-section/contacts-section/download-resume-btn.tsx
+++ src/components/website/profile-side-section/contacts-section/download-resume-btn.tsx
@@ -1,9 +1,13 @@
+import { Download } from 'lucide-react';
 import React from 'react';
 
 const DownloadResumeBtn: React.FC = () => {
   return (
     <button className="main-gradient-1 px-4 py-2 rounded-full">
-      DownloadResumeBtn
+      <div className="flex items-center justify-center gap-2">
+        <Download size={16} />
+        <p className="para-2 font-semibold">Download Resume</p>
+      </div>
     </button>
   );
 };
