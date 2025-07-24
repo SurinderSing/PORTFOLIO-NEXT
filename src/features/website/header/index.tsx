1a287b564f4486f6d37fd5104142393c6bb5c66d
Surinder Singh <ssurindersingh100@gmail.com>
2025-07-24 15:46:01 +0530

    refactor: adjust layout styles for consistency across auth and website sections
    - Removed unnecessary padding from the auth layout and adjusted the padding in the website layout for better alignment.
    - Simplified the header component by removing fixed positioning to improve layout flexibility.

--- src/features/website/header/index.tsx
+++ src/features/website/header/index.tsx
@@ -4,7 +4,7 @@ import LogoHeading from '@/components/website/logo-heading';
 
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 w-full z-50 bg-background">
+    <header className="bg-background">
       <div className="section-container">
         <div className="flex justify-between items-center mx-auto py-1.5 sm:py-1">
           <LogoHeading title="Inder <span>Dev.</span>" href="/" />
