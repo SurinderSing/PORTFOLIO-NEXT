e18118fabf00a3906d96acb3a6f724ba12139089
Surinder Singh <ssurindersingh100@gmail.com>
2025-07-24 16:48:24 +0530

    feat: add profile photo for Surinder Singh in photo section
    - Introduced a new profile photo for Surinder Singh, enhancing the visual representation in the photo section.
    - Updated the image source to utilize the newly added profile photo, ensuring a personalized touch.

--- src/components/website/profile-side-section/photo-section/index.tsx
+++ src/components/website/profile-side-section/photo-section/index.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Image from 'next/image';
 import React, { useState } from 'react';
+import Photo from '@/assets/images/profile-photos/surinder_profile_photo.jpg';
 
 const PhotoSection: React.FC = () => {
   const [imageError, setImageError] = useState(false);
@@ -16,12 +17,12 @@ const PhotoSection: React.FC = () => {
           src={
             imageError
               ? 'https://images.unsplash.com/photo-1614027164847-1b28cfe1df60?q=80&w=1986&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-              : 'https://instagram.fdel52-1.fna.fbcdn.net/v/t51.29350-15/412307197_954015226286467_765481920037607741_n.webp?stp=dst-jpg_e35_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE0NDAuc2RyLmYyOTM1MC5kZWZhdWx0X2ltYWdlIn0&_nc_ht=instagram.fdel52-1.fna.fbcdn.net&_nc_cat=107&_nc_oc=Q6cZ2AGFDJ-qUaOJiXK_yXExopsNkgFnhiJ0mOA4GYaihgb61pOrrFM_KgUl6zdrT7FpZMnjIZQWyALSnSFrfTo3I0rK&_nc_ohc=C4ojDzyd84UQ7kNvgHH3Kxx&_nc_gid=0e286f9744984b079d668058a684bfc0&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=MzI2MTM5ODA2NDkzNzQ4MzY1NA%3D%3D.3-ccb7-5&oh=00_AYBRNVcKLXY4LrRscWwLuslVrAMz6XqsLlDC0CGI6yyriA&oe=67CCC3CC&_nc_sid=fc8dfb'
+              : Photo
           }
           alt="Surinder Singh - Frontend Developer"
           width={150}
           height={150}
-          className="object-cover aspect-square rounded-2xl absolute -top-[80px]"
+          className="object-cover aspect-square rounded-full absolute -top-[82px]"
           onError={handleImageError}
           priority
         />
